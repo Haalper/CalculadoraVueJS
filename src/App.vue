a3c319d39d427ac3a735d0c811a6058c76dfc589
Halisson Nascimento <halissonp@hotmail.com>
2024-08-04 22:48:30 -0300

    Adicionar projeto calculadora-vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="calculator">
+    <input type="number" v-model.number="num1" @input="calculate" class="input-field">
+    <select v-model="operator" @change="calculate" class="operator-select">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+    <input type="number" v-model.number="num2" @input="calculate" class="input-field">
+    <div class="result">
+      Resultado: {{ result }}
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operator: '+',
+      result: 0
+    };
+  },
+  methods: {
+    calculate() {
+      switch (this.operator) {
+        case '+':
+          this.result = this.num1 + this.num2;
+          break;
+        case '-':
+          this.result = this.num1 - this.num2;
+          break;
+        case '*':
+          this.result = this.num1 * this.num2;
+          break;
+        case '/':
+          this.result = this.num1 / this.num2;
+          break;
+        default:
+          this.result = 0;
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.calculator {
+  max-width: 300px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.input-field {
+  width: calc(50% - 22px);
+  padding: 8px;
+  margin: 10px;
+  font-size: 16px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+
+.operator-select {
+  padding: 8px;
+  margin: 10px;
+  font-size: 16px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+
+.result {
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 20px;
 }
 </style>
